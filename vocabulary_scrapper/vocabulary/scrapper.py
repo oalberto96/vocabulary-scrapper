b7a6fb9ef321d7360876b1477383ac1a86c70b00
oalberto96 <ochoapublic@gmail.com>
2019-02-09 18:27:17 -0800

    finish scrapper and post lesson from result.json

--- vocabulary_scrapper/vocabulary/scrapper.py
+++ vocabulary_scrapper/vocabulary/scrapper.py
@@ -32,10 +32,22 @@ class DWSpider(scrapy.Spider):
         yield data
 
 
+def run(url):
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': "result.json",
+    })
+    process.crawl(DWSpider, url)
+    # the script will block here until the crawling is finished
+    process.start(stop_after_crawl=False)
+
+
 if __name__ == "__main__":
     process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': "result.json",
     })
-
     process.crawl(DWSpider, sys.argv[1])
     process.start()  # the script will block here until the crawling is finished
--- vocabulary_scrapper/vocabulary/views.py
+++ vocabulary_scrapper/vocabulary/views.py
@@ -1,14 +1,62 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.conf import settings
+
+import json
+import os
+import requests
 
 # Create your views here
 
 
+def read_result():
+    with open(os.path.join(settings.MEDIA_ROOT, 'result.json')) as file:
+        data = json.load(file)
+    return data
+
+
 def index(request):
     template = loader.get_template('vocabulary/index.html')
-    return HttpResponse(template.render({}, request))
+    result = read_result()
+    print(result)
+    return HttpResponse(template.render(result[0], request))
 
 
 def extract(request):
-    print(request.POST)
+    credentials = {
+        'username': request.POST['username'],
+        'password': request.POST['password']
+    }
+    cookies = requests.post(
+        'http://flashycards.co/api/authentication/login/', data=credentials).json()
+    headers = {
+        'Authorization': "Token " + cookies['token'],
+        "Content-Type": "application/json; charset=utf8"
+    }
+    result = read_result()
+    lesson = prepare_lesson(result[0])
+    r = requests.post(
+        'http://flashycards.co/api/lessons/lessons/',
+        data=json.dumps(lesson),
+        headers=headers
+    )
+    return HttpResponseRedirect("/")
+
+
+def prepare_lesson(result):
+    lesson = {}
+    lesson['name'] = result['title']
+    lesson['description'] = 'from dw'
+    lesson['concepts'] = []
+    lesson['audience'] = {'id': 1}
+    id = 0
+    for row in result['data']:
+        concept = {}
+        concept['id'] = id - 1
+        concept['card_a'] = {'text': row['german'],
+                             'audio': row['audio'], 'media': None}
+        concept['card_b'] = {'text': row['english'], 'media': None}
+        lesson['concepts'].append(concept)
+    lesson['concepts'].pop()
+    return lesson
